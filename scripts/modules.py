662f3bdb8cefa075dbacdcfb44affb9be8da7b10
patdz <patdz@ymipro.com>
2022-07-17 14:48:29 +0800

    u

--- scripts/modules.py
+++ scripts/modules.py
@@ -1,16 +1,22 @@
 import fileinput
 import os
 
+modules = []
 
 scriptPath = os.path.split(os.path.realpath(__file__))[0]
-cfgFile = os.path.join(scriptPath, "..", "local.cfg")
+cfgFile = os.path.join(scriptPath, "..", ".modules")
 
 if os.path.isfile(cfgFile):
     for line in fileinput.input(cfgFile):
         if len(line) == 0 or line.startswith('#'):
             continue
-        parts = line.split('=', 2)
-        if len(parts) != 2:
+        m = line.strip()
+        if m.startswith('\'') and m.endswith('\''):
+            m = m.strip('\'')
+        if m.startswith('"') and m.endswith('"'):
+            m = m.strip('"')
+        if m == '':
             continue
-        exec(parts[0] + ' = ' + parts[1].capitalize())
-
+        modules.append(m)
+else:
+    modules = ['nginx', 'php', 'redis', 'mysql_server']
